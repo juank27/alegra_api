9c4ddc3095dc7c1340078edaa7c9ff115b750155
juank27 <juankruiz27@gmail.com>
2023-12-01 17:13:19 -0500

    Reqeust finish and test DIAN

--- server.js
+++ server.js
@@ -21,17 +21,23 @@ app.use(authMiddleware);
 let token = loadTokens();
 
 token[
-  process.env.DEVELOPMENT ? process.env.EMAIL_AUTH_DEV : process.env.EMAIL_AUTH
+  process.env.DEVELOPMENT == "true"
+    ? process.env.EMAIL_AUTH_DEV
+    : process.env.EMAIL_AUTH
 ] = encryptToken(
-  process.env.DEVELOPMENT ? process.env.TOKEN_AUTH_DEV : process.env.TOKEN_AUTH
+  process.env.DEVELOPMENT == "true"
+    ? process.env.TOKEN_AUTH_DEV
+    : process.env.TOKEN_AUTH
 );
 
 saveTokens(token, "admin");
 
-const HOST = process.env.DEVELOPMENT
-  ? process.env.ENDPOINT_DEV
-  : process.env.ENDPOINT;
+const HOST =
+  process.env.DEVELOPMENT == "true"
+    ? process.env.ENDPOINT_DEV
+    : process.env.ENDPOINT;
 
+console.log("🚀 ~ file: server.js:32 ~ HOST:", HOST);
 const PORT = process.env.PORT || 3000;
 
 // Ruta del archivo de registro
@@ -159,12 +165,14 @@ app.post("/generar-token", (req, res) => {
   const email = data.email;
 
   // Verificar las credenciales del admin
-  const adminEmail = process.env.DEVELOPMENT
-    ? process.env.EMAIL_AUTH_DEV
-    : process.env.EMAIL_AUTH;
-  const adminToken = process.env.DEVELOPMENT
-    ? process.env.TOKEN_AUTH_DEV
-    : process.env.TOKEN_AUTH;
+  const adminEmail =
+    process.env.DEVELOPMENT == "true"
+      ? process.env.EMAIL_AUTH_DEV
+      : process.env.EMAIL_AUTH;
+  const adminToken =
+    process.env.DEVELOPMENT == "true"
+      ? process.env.TOKEN_AUTH_DEV
+      : process.env.TOKEN_AUTH;
 
   // Obtener el token del encabezado de autorización
   const authHeader = req.header("Authorization");
@@ -196,110 +204,244 @@ app.post("/generar-token", (req, res) => {
 });
 
 app.post("/upload", upload.single("data"), (req, res) => {
-  handleCSVUpload(req, res)
-    .then((filePathName) => {
-      deleteFilesInFolder(uploadsFolderPath);
-      if (filePathName.length <= 1) {
-        return res.status(400).json({
-          message: "El archivo no tiene datos.",
-        });
-      }
-      if (filePathName.length > 100) {
-        return res.status(400).json({
-          message:
-            "El archivo tiene más de 100 registros. Excede el límite para procesar.",
-        });
-      }
-      const validationErrors = validateFields(filePathName);
-      if (validationErrors.length > 0) {
-        return res.status(400).json({
-          message: "El archivo tiene errores en los siguientes campos:",
-          errors: validationErrors,
-        });
-      }
-      const groupedData = groupObjectsById(filePathName);
-      getNumberTemplate()
-        .then((response) => {
-          const idNumeration = response.data
-            .filter((obj) => obj.name === "Documento soporte")
-            .map((obj) => obj.id);
-          console.log(
-            "🚀 ~ file: server.js:226 ~ .then ~ idNumeration:",
-            typeof idNumeration[0]
-          );
-          const objectRequest = transformData(groupedData, idNumeration[0]);
-          return res.status(200).json({
-            message: "Archivo cargado correctamente.",
-            filePath: objectRequest,
+  handleCSVUpload(req, res).then((filePathName) => {
+    console.log(
+      "🚀 ~ file: server.js:200 ~ handleCSVUpload ~ filePathName:",
+      filePathName
+    );
+    deleteFilesInFolder(uploadsFolderPath);
+    if (filePathName.length < 1) {
+      return res.status(400).json({
+        message: "El archivo no tiene datos.",
+      });
+    }
+    if (filePathName.length > 100) {
+      return res.status(400).json({
+        message:
+          "El archivo tiene más de 100 registros. Excede el límite para procesar.",
+      });
+    }
+    const validationErrors = validateFields(filePathName);
+    if (validationErrors.length > 0) {
+      return res.status(400).json({
+        message: "El archivo tiene errores en los siguientes campos:",
+        errors: validationErrors,
+      });
+    }
+    const groupedData = groupObjectsById(filePathName);
+    getNumberTemplate()
+      .then((response) => {
+        const idNumeration = response.data
+          .filter(
+            (obj) =>
+              obj.documentType === "supportDocument" &&
+              obj.status === "active" &&
+              obj.isDefault === true
+          )
+          // .filter((obj) => obj.name === "Documento soporte")
+          .map((obj) => ({ id: obj.id, nextNumber: obj.nextInvoiceNumber }));
+
+        const objectRequest = transformData(
+          groupedData,
+          idNumeration[0].id,
+          idNumeration[0].nextNumber
+        );
+        // const objectRequest = transformData(groupedData, "10");
+        console.log(
+          "🚀 ~ file: server.js:234 ~ .then ~ objectRequest:",
+          objectRequest
+        );
+        const pageSize = 2;
+        const i = 0;
+        sendBatchPromise(i, objectRequest)
+          .then((errorData) => {
+            if (errorData.length > 0) {
+              return res.status(500).json({
+                message:
+                  "Error al generar el documento soporte para los siguietes ids:",
+                errors: errorData,
+              });
+            }
+            return res.status(200).json({
+              message: "Archivo cargado correctamente.",
+            });
+          })
+          .catch((error) => {
+            console.log("Error al enviar el batch:", error);
+            console.log(
+              "Error al enviar el batch: -------------------------------",
+              error.response.data
+            );
+            return res.status(500).json(error.response.data);
           });
-        })
-        .catch((error) => {
-          console.log("Error al obtener el id de numeracion", error);
-          return res.status(500).json(error);
-        });
-    })
-    .catch((error) => {
-      console.log("Error al analizar el archivo CSV:", error);
-      deleteFilesInFolder(uploadsFolderPath);
-      return res.status(500).json(error);
-    });
+        // objectRequest.forEach((item) => {
+        //   sendBatch(item)
+        //     .then((response) => {
+        //       console.log("Batch sent successfully:", response.data);
+        //       i++;
+        //     })
+        //     .catch((error) => {
+        //       console.log("Error al enviar el batch:", error);
+        //       console.log(
+        //         "Error al enviar el batch: -------------------------------",
+        //         error.response.data
+        //       );
+        //       return res.status(500).json(error.response.data);
+        //     });
+        // });
+        // if (objectRequest.length === i) {
+        //   return res.status(200).json({
+        //     message: "Archivo cargado correctamente.",
+        //     filePath: objectRequest,
+        //   });
+        // } else {
+        //   return res.status(500).json({
+        //     message: "Error al enviar el batch.",
+        //   });
+        // }
+        // Llamada a la función para enviar datos paginados
+        //   sendPaginatedData(objectRequest, pageSize)
+        //     .then((responses) => {
+        //       responses.forEach((response) => {
+        //         console.log("Batch sent successfully:", response.data);
+        //       });
+        //     })
+        //     .then(() => {
+        //       return res.status(200).json({
+        //         message: "Archivo cargado correctamente.",
+        //         filePath: objectRequest,
+        //       });
+        //     })
+        //     .catch((error) => {
+        //       console.log("🚀 ~ file: server.js:246 ~ .then ~ error:", error);
+        //       console.error(
+        //         "Error sending paginated data:",
+        //         error.response.data
+        //       );
+        //       return res.status(500).json(error.response.data);
+        //     });
+        //   // return res.status(200).json({
+        //   //   message: "Archivo cargado correctamente.",
+        //   //   filePath: objectRequest,
+        //   // });
+        // })
+        // .catch((error) => {
+        //   console.log("Error al obtener el id de numeracion", error);
+        //   return res.status(500).json(error);
+        // });
+      })
+      .catch((error) => {
+        console.log("Error al analizar el archivo CSV:", error);
+        deleteFilesInFolder(uploadsFolderPath);
+        return res.status(500).json(error);
+      });
+  });
 });
 
-app.get("/", (req, res) => {
-  const options = {
-    method: "POST",
-    url: "https://sandbox.alegra.com:26967/api/v1/bills",
-    headers: {
-      accept: "application/json",
-      "content-type": "application/json",
-      authorization:
-        "Basic YWVyb3JlbnRhbCthbGVncmFAYWxlZ3JhLmNvbTo5YzBjNWU3MTExYTIwMjkyZjAyNA==",
-    },
-    data: {
-      numberTemplate: { id: "10" },
-      purchases: {
-        items: [
-          {
-            id: 118,
-            name: "",
-            discount: 0,
-            quantity: 1,
-            observations: "Billetera de cuero negro",
-            price: 1,
-            subtotal: null,
-            tax: [],
-          },
-          {
-            id: 118,
-            name: "",
-            discount: 0,
-            observations: "otraaaa",
-            quantity: 1,
-            tax: [],
-            price: 70,
-          },
-        ],
-      },
-      currency: { code: "USD", symbol: "$", exchangeRate: 2950 },
-      paymentType: "CASH",
-      billOperationType: "INDIVIDUAL",
-      date: "2023-02-11",
-      dueDate: "2023-02-12",
-      provider: 15,
-      // retentions: [],
-    },
-  };
+app.post("/", (req, res) => {
+  getNumberTemplate()
+    .then((response) => {
+      const idNumeration = response.data
+        .filter(
+          (obj) =>
+            obj.documentType === "supportDocument" &&
+            obj.status === "active" &&
+            obj.isDefault === true
+        )
+        .map((obj) => ({ id: obj.id, nextNumber: obj.nextInvoiceNumber }));
+      console.log(
+        "🚀 ~ file: server.js:273 ~ .then ~ idNumeration:",
+        idNumeration
+      );
 
-  axios
-    .request(options)
-    .then(function (response) {
-      // console.log(JSON.parse(response.data));
-      return res.json(response.data);
+      return res.status(200).json(response.data);
     })
-    .catch(function (error) {
-      console.error(error);
-      return res.json(error);
+    .catch((error) => {
+      console.log("Error al obtener el id de numeracion", error);
+      return res.status(500).json(error);
     });
+  // data: {
+  //   numberTemplate: { id: "20" },
+  //   purchases: {
+  //     items: [
+  //       {
+  //         id: 118,
+  //         name: "",
+  //         discount: 0,
+  //         quantity: 1,
+  //         observations: "Billetera de cuero negro",
+  //         price: 1,
+  //         subtotal: null,
+  //         tax: [],
+  //       },
+  //       {
+  //         id: 118,
+  //         name: "",
+  //         discount: 0,
+  //         observations: "otraaaa",
+  //         quantity: 1,
+  //         tax: [],
+  //         price: 1,
+  //       },
+  //     ],
+  //   },
+  //   // currency: { code: "USD", symbol: "$", exchangeRate: 2950 },
+  //   paymentType: "CASH",
+  //   paymentMethod: "CASH",
+  //   billOperationType: "INDIVIDUAL",
+  //   date: "2023-29-11",
+  //   dueDate: "2023-05-12",
+  //   provider: 679,
+  //   // retentions: [],
+  //   stamp: { generateStamp: true },
+  // },
+
+  // const options = {
+  //   method: "POST",
+  //   url: "https://sandbox.alegra.com:26967/api/v1/bills",
+  //   headers: {
+  //     accept: "application/json",
+  //     "content-type": "application/json",
+  //     authorization:
+  //       "Basic YWVyb3JlbnRhbCthbGVncmFAYWxlZ3JhLmNvbTo5YzBjNWU3MTExYTIwMjkyZjAyNA==",
+  //   },
+  //   data: {
+  //     numberTemplate: { id: "20" },
+  //     purchases: {
+  //       items: [
+  //         {
+  //           id: 118,
+  //           discount: 0,
+  //           observations: "Billetera de cuero negro",
+  //           price: 1,
+  //           quantity: 1,
+  //           tax: [],
+  //         },
+  //       ],
+  //     },
+  //     stamp: { generateStamp: true },
+  //     paymentType: "CASH",
+  //     billOperationType: "INDIVIDUAL",
+  //     date: "2023-11-30",
+  //     dueDate: "2023-12-05",
+  //     observations: "test subida newwwww",
+  //     provider: 679,
+  //     paymentMethod: "CASH",
+  //     retentions: [],
+  //   },
+  // };
+
+  // axios
+  //   .request(options)
+  //   .then(function (response) {
+  //     // console.log(JSON.parse(response.data));
+  //     return res.json(response.data);
+  //   })
+  //   .catch(function (error) {
+  //     console.error(error);
+  //     console.error(error.response.data);
+  //     return res.json(error.response.data);
+  //   });
   // res.send("Hello AeroRental!");
 });
 
@@ -314,9 +456,9 @@ const validateFields = (data) => {
     "purchases_quantity",
     "purchases_total",
     // "prchases_subtotal",
-    "currency_code",
-    "currency_symbol",
-    "currency_exhangeRate",
+    // "currency_code",
+    // "currency_symbol",
+    // "currency_exhangeRate",
     "paymentMethod",
     "paymentType",
     "billOperationType",
@@ -370,22 +512,11 @@ const getNumberTemplate = () => {
   return axios.request(options);
 };
 
-const createBill = (data) => {
-  const auth = credentialsBase64();
-  const options = {
-    method: "POST",
-    url: HOST + "/api/v1/bills",
-    headers: {
-      accept: "application/json",
-      "content-type": "application/json",
-      authorization: "Basic " + auth,
-    },
-  };
-};
 const credentialsBase64 = () => {
-  const credentials = process.env.DEVELOPMENT
-    ? process.env.EMAIL_DEV + ":" + process.env.TOKEN_DEV
-    : process.env.EMAIL + ":" + process.env.TOKEN;
+  const credentials =
+    process.env.DEVELOPMENT == "true"
+      ? process.env.EMAIL_DEV + ":" + process.env.TOKEN_DEV
+      : process.env.EMAIL + ":" + process.env.TOKEN;
   const base64Credentials = Buffer.from(credentials, "utf-8").toString(
     "base64"
   );
@@ -393,8 +524,13 @@ const credentialsBase64 = () => {
 };
 
 // Función para transformar un objeto individual según el formato requerido
-const transformObject = (originalObject, numeration) => {
+const transformObject = (originalObject, numeration, nextNumber) => {
+  console.log(
+    "🚀 ~ file: server.js:524 ~ transformObject ~ originalObject:",
+    originalObject
+  );
   const transformedObject = {
+    idExterno: originalObject.id,
     numberTemplate: { id: numeration },
     purchases: {
       items: originalObject.objects.map((item) => {
@@ -405,10 +541,12 @@ const transformObject = (originalObject, numeration) => {
           observations: item.purchases_observations,
           quantity: parseInt(item.purchases_quantity),
           price: parseFloat(item.purchases_price),
-          total: item.purchases_total ? parseFloat(item.purchases_total) : null,
+          total: item.purchases_total
+            ? parseFloat(item.purchases_total)
+            : item.purchases_total,
           subtotal: item.purchases_subtotal
             ? parseFloat(item.purchases_subtotal)
-            : null,
+            : item.purchases_subtotal,
           tax: item.tax_id
             ? [
                 {
@@ -428,10 +566,13 @@ const transformObject = (originalObject, numeration) => {
     currency: {
       code: originalObject.objects[0].currency_code,
       symbol: originalObject.objects[0].currency_symbol,
-      exchangeRate: parseFloat(originalObject.objects[0].currency_exhangeRate),
+      exchangeRate: originalObject.objects[0].currency_exhangeRate
+        ? parseFloat(originalObject.objects[0].currency_exhangeRate)
+        : "",
     },
     paymentType: originalObject.objects[0].paymentType,
     billOperationType: originalObject.objects[0].billOperationType,
+    paymentMethod: originalObject.objects[0].paymentMethod,
     date: originalObject.objects[0].date,
     dueDate: originalObject.objects[0].dueDate,
     provider: parseInt(originalObject.objects[0].provider),
@@ -443,7 +584,9 @@ const transformObject = (originalObject, numeration) => {
     //   };
     // }),
     retentions: [],
-    stamp: { generateStamp: Boolean(originalObject.objects[0].stamp) },
+    stamp: {
+      generateStamp: originalObject.objects[0].stamp == "false" ? false : true,
+    },
   };
   // Agregar retentions solo si hay datos
   originalObject.objects.forEach((item) => {
@@ -458,7 +601,7 @@ const transformObject = (originalObject, numeration) => {
 };
 
 // Función para transformar el array completo
-const transformData = (originalData, numeration) => {
+const transformData = (originalData, numeration, nextNumber) => {
   const transformedData = [];
 
   originalData.forEach((originalObject) => {
@@ -473,12 +616,93 @@ const transformData = (originalData, numeration) => {
       return;
     }
 
-    const transformedObject = transformObject(originalObject, numeration);
+    const transformedObject = transformObject(
+      originalObject,
+      numeration,
+      nextNumber
+    );
     transformedData.push(transformedObject);
   });
 
   return transformedData;
 };
+
+// Función para enviar un lote de datos
+const sendBatch = (dataBatch) => {
+  console.log(
+    "🚀 ~ file: server.js:520 ~ sendBatch ~ dataBatch:",
+    JSON.stringify(dataBatch)
+  );
+  const auth = credentialsBase64();
+  const options = {
+    method: "POST",
+    url: HOST + "/api/v1/bills",
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      authorization: "Basic " + auth,
+    },
+    data: JSON.stringify(dataBatch),
+  };
+
+  return axios.request(options);
+};
+const sendBatchPromise = (index, objectRequest) => {
+  const errorData = [];
+
+  return new Promise((resolve, reject) => {
+    const enviar = () => {
+      if (index < objectRequest.length) {
+        sendBatch(objectRequest[index])
+          .then((response) => {
+            console.log("Batch sent successfully:", response.data);
+            // Llamar a la próxima iteración
+            enviar(index++);
+          })
+          .catch((error) => {
+            console.log(
+              "Error al enviar el batch: -------------------------------",
+              error.response.data
+            );
+            errorData.push({
+              id: parseInt(objectRequest[index].idExterno),
+              message: error.response.data.error
+                ? error.response.data.error
+                : error.response.data,
+              id_alegra: error.response.data.bill
+                ? parseInt(error.response.data.bill.id)
+                : null,
+            });
+            // Llamar a la próxima iteración
+            enviar(index++);
+          });
+      } else {
+        // El código aquí se ejecutará después de que se resuelvan todas las promesas.
+        console.log("Todas las promesas se completaron");
+        // Resuelve la promesa principal con los datos de error
+        resolve(errorData);
+      }
+    };
+    // Iniciar el bucle llamando a la función con índice 0
+    enviar();
+  });
+};
+
+// Función para enviar datos paginados utilizando Promise.all
+const sendPaginatedData = async (dataArray, pageSize) => {
+  const totalItems = dataArray.length;
+  const promises = [];
+
+  for (let startIndex = 0; startIndex < totalItems; startIndex += pageSize) {
+    const endIndex = Math.min(startIndex + pageSize, totalItems);
+    const dataBatch = dataArray.slice(startIndex, endIndex);
+    const promise = sendBatch(dataBatch);
+    promises.push(promise);
+  }
+
+  return Promise.all(promises);
+};
+
 //_----------------------------------------------------------------------------------------------
 app.get("/api/bill", (req, res) => {
   const filePath = "./data.csv";
